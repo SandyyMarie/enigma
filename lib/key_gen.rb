1710aa4112ff17db399ca9d06826088c7ab57e9d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-14 01:10:27 -0600

    test: added additional test to key_gen with passing random key generator

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,5 +1,10 @@
+require 'securerandom'
 class Key_Gen
+
+  attr_reader :new_key
   def initialize
     #("a".."z").to_a << " "
+    @new_key = SecureRandom.random_number(99999)
+    # require 'pry' ; binding.pry
   end
 end
--- spec/key_gen_spec.rb
+++ spec/key_gen_spec.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require './lib/enigma'
+require './lib/key_gen'
 require 'date'
 
 RSpec.describe Key_Gen do
@@ -11,3 +12,8 @@ RSpec.describe Key_Gen do
   it 'exists & has attributes' do
     expect(@key_generator).to be_a(Key_Gen)
   end
+
+  it "generates 5 integers" do
+    expect(@key_generator.new_key.digits.count).to eq(5)
+  end
+end
